9e22cb129c6e1e4ade6d91b971cbc496d187432f
sanal m s <131573751+sanal-m-s@users.noreply.github.com>
2024-05-03 12:34:04 +0530

    Update EmployeeDetails.java

--- OOPS/EmployeeDetails.java
+++ OOPS/EmployeeDetails.java
@@ -1,14 +1,11 @@
 import java.util.Scanner;
-
 class EmployeeDetails{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the number of employees: ");
         int n = scanner.nextInt();
         scanner.nextLine();
         Employee[] employees = new Employee[n];
-
         for (int i = 0; i < n; i++) {
             System.out.println("Enter details for Employee " + (i + 1) + ":");
             System.out.print("Employee Number: ");
@@ -21,7 +18,6 @@ class EmployeeDetails{
             scanner.nextLine();
             employees[i] = new Employee(eNo, eName, eSalary);
         }
-
         System.out.print("Enter the employee number to search: ");
         int searchENo = scanner.nextInt();
         boolean found = false;
@@ -32,11 +28,9 @@ class EmployeeDetails{
                 break;
             }
         }
-
         if (!found) {
             System.out.println("Employee with employee number " + searchENo + " not found.");
         }
-
         scanner.close();
     }
 }
@@ -44,13 +38,11 @@ class Employee {
     int eNo;
     String eName;
     int eSalary;
-
     Employee(int eNo, String eName, int eSalary) {
         this.eNo = eNo;
         this.eName = eName;
         this.eSalary = eSalary;
     }
-
     void display() {
         System.out.println("Employee Number: " + eNo);
         System.out.println("Employee Name: " + eName);
