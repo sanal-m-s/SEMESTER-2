3ce3850152be7f14d69cac7bded50fbf63fe1154
sanal m s <131573751+sanal-m-s@users.noreply.github.com>
2024-06-07 18:07:16 +0530

    Update Calculator.java

--- OOPS/calculator package/com/mycompanymath/Calculator.java
+++ OOPS/calculator package/com/mycompanymath/Calculator.java
@@ -3,7 +3,7 @@ public class Calculator{
     public int add(int a, int b){
         return a + b;
     }
-    public int subtract(int a, int b) {
+    public int subtract(int a, int b){
         return a - b;
     }
 
