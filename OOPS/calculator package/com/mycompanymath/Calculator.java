2470065e273be9cfb1d7fe74dc03e67cc7b61ea2
sanal m s <131573751+sanal-m-s@users.noreply.github.com>
2024-06-07 18:09:22 +0530

    Update Calculator.java

--- OOPS/calculator package/com/mycompanymath/Calculator.java
+++ OOPS/calculator package/com/mycompanymath/Calculator.java
@@ -9,7 +9,7 @@ public class Calculator{
     public int multiply(int a, int b){
         return a * b;
     }
-    public double divide(double a, double b) {
+    public double divide(double a, double b){
         if (b == 0) {
             throw new ArithmeticException("Cannot divide by zero");
         }
